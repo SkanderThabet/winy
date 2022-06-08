58b2d3aab74dd8e95a53c2eed13be134e6d46eb7
SkanderThabet <skander.thabet@esprit.tn>
2022-06-08 13:27:00 +0100

    Consuming Icon Fonts in App, Creating Simple Custom, Reusable widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: SplashPage(),
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SplashPage(duration: 3, goToPage: WelcomePage(),),
   ));
 }
+
+
+
 class SplashPage extends StatelessWidget {
+  int duration = 0;
+  Widget goToPage;
+
+
+  SplashPage({Key? key, required this.duration, required this.goToPage}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: duration), () {
+      Navigator.push(context, MaterialPageRoute(builder: (context) => goToPage));
+    });
+
     return Scaffold(
       body: Container(
-    color: const Color(0xFF80C038),
-  alignment: Alignment.center,
-    ),
+          color: const Color(0xFFFCFCFC),
+          alignment: Alignment.center,
+          child:
+              Image.asset(
+                  'assets/imgs/Asset 1.png', width: 150,height: 150,))
     );
   }
 }
 
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    body: Container(
+      alignment: Alignment.center,
+      child: Text('Hello World ! '),
+    ),
+    );
+
+  }
+}
 
